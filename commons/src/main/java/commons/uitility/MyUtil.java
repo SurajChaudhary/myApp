fdfa9e05bcdc8ac5d3bda37681bd27996b34bdf0
Suraj Chaudhary <chaudharysurajsingh@gmail.com>
2018-11-01 16:23:31 +0530

    test commit

---
+++ commons/src/main/java/commons/uitility/MyUtil.java
@@ -0,0 +1,9 @@
+package commons.uitility;
+
+public class MyUtil {
+	
+	public static int getLength(String input){
+		return (input != null) ? input.length() : 0;
+	}
+
+}
---
+++ services/src/main/java/services/api/MyService.java
@@ -0,0 +1,13 @@
+package services.api;
+
+import commons.uitility.MyUtil;
+
+public class MyService {
+
+	public static void main(String[] args) {
+		String ss = "ahgjhgjgjg";
+		System.out.println(MyUtil.getLength(ss));
+
+	}
+
+}
